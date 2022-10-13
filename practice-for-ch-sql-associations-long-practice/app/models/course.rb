ee6da3a0b9f0b9558403eb0cbb46248f42a08825
Arnob <arnobdam@gmail.com>
2022-10-13 16:11:47 -0400

    added course associations

--- practice-for-ch-sql-associations-long-practice/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice/app/models/course.rb
@@ -10,4 +10,21 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+
+    belongs_to :prerequisite,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course
+
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_students,
+        through: :enrollments,
+        source: :student
+
+
+
 end
--- practice-for-ch-sql-associations-long-practice/app/models/enrollment.rb
+++ practice-for-ch-sql-associations-long-practice/app/models/enrollment.rb
@@ -15,7 +15,7 @@ class Enrollment < ApplicationRecord
         foreign_key: :course_id,
         class_name: :Course
 
-    has_many :students,
+    belongs_to :student,
         primary_key: :id,
         foreign_key: :student_id,
         class_name: :User
